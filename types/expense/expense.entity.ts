e49b12ba0cd09e2c48fb70bfcdc6745524fcd38e
Paweł Jarecki <paweljarecki10@gmail.com>
2024-02-27 18:19:26 +0100

    New interface for the tic tac toe game

--- types/expense/expense.entity.ts
+++
@@ -1,8 +0,0 @@
-export interface ExpenseEntity {
-    id?: string,
-    category: string,
-    name: string,
-    cost: number,
-    month: string,
-    notes: string,
-}
---
+++ types/match/match.entity.ts
@@ -0,0 +1,8 @@
+export interface MatchEntity {
+    id?: string,
+    player1: string,
+    player2: string,
+    gameTime: number,
+    winner: string,
+    numberOfMoves: string,
+}
