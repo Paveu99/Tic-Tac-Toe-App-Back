81f2a0b6995c0e597fd95bc6d18a259b0d45194b
Paweł Jarecki <paweljarecki10@gmail.com>
2024-03-03 13:44:20 +0100

    matchRouter added to the index.ts file

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, {json} from "express"
 import cors from "cors"
 import {handleError} from "./utils/errors";
+import {matchRouter} from "./routers/match";
 
 const app = express()
 
@@ -9,6 +10,8 @@ app.use(cors({
 }));
 
 app.use(json());
+
+app.use('/match', matchRouter)
 
 app.use(handleError)
 
