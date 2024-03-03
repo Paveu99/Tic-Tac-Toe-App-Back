926b072b19b77a28af3c1c0701480cb2cd18d766
Paweł Jarecki <paweljarecki10@gmail.com>
2024-03-03 13:10:06 +0100

    Record for handling connection to the database. Initial state

--- records/match.record.ts
+++ records/match.record.ts
@@ -4,140 +4,43 @@ import {FieldPacket} from "mysql2"
 import {v4 as uuid} from "uuid"
 import { format } from 'date-fns';
 
-interface SummaryMonth {
-    sum: number,
-    categoryMost: string,
-    categoryLeast: string,
-    latest: string,
-    cost: number,
-    maxAmountCat: number,
-    minAmountCat: number,
-}
+type MatchRecordResults =[MatchRecord[], FieldPacket[]];
 
-interface Summary extends SummaryMonth{
-    monthMost: string,
-    monthLeast: string,
-    maxAmountMonth: number,
-    minAmountMonth: number,
-}
-
-type ExpensesRecordResults =[MatchRecord[], FieldPacket[]];
-type SummaryResults =[Summary[], FieldPacket[]];
-type SummaryResultsMonth =[SummaryMonth[], FieldPacket[]];
-
-export class MatchRecord implements ExpenseEntity {
-    public category: string;
-    public cost: number;
+export class MatchRecord implements MatchEntity {
+    public player1: string;
+    public player2: string;
+    public gameTime: number;
+    public numberOfMoves: number;
     public id: string;
-    public month: string;
-    public name: string;
-    public notes: string;
+    public date: string;
+    public winner: string;
 
-    constructor(obj: ExpenseEntity) {
+    constructor(obj: MatchEntity) {
         this.id = obj.id;
-        this.category = obj.category;
-        this.name = obj.name;
-        this.cost = obj.cost;
-        this.month = obj.month;
-        this.notes = obj.notes;
+        this.player1 = obj.player1;
+        this.player2 = obj.player2;
+        this.gameTime = obj.gameTime;
+        this.numberOfMoves = obj.numberOfMoves;
+        this.winner = obj.winner;
+        this.date = obj.date;
     };
 
-    static async getSummary(): Promise<Summary> {
-        const [sum] = await pool.execute("SELECT SUM(cost) AS sum FROM `spendings`") as SummaryResults;
-        const [latest] = await pool.execute("SELECT name as latest FROM `spendings` ORDER BY month DESC LIMIT 1") as SummaryResults;
-        const [monthMost] = await pool.execute("SELECT CONCAT(MONTHNAME(MAX(month)), ' ', YEAR(MAX(month))) AS monthMost, SUM(cost) AS maxAmountMonth FROM `spendings` GROUP BY YEAR(month), MONTH(month) ORDER BY maxAmountMonth DESC LIMIT 1") as SummaryResults;
-        const [monthLeast] = await pool.execute("SELECT CONCAT(MONTHNAME(MAX(month)), ' ', YEAR(MAX(month))) AS monthLeast, SUM(cost) AS minAmountMonth FROM `spendings` GROUP BY YEAR(month), MONTH(month) ORDER BY minAmountMonth ASC LIMIT 1") as SummaryResults;
-        const [categoryMost] = await pool.execute("SELECT category AS categoryMost, SUM(cost) AS maxAmountCat FROM `spendings` GROUP BY category ORDER BY maxAmountCat DESC LIMIT 1") as SummaryResults;
-        const [categoryLeast] = await pool.execute("SELECT category AS categoryLeast, SUM(cost) AS minAmountCat FROM `spendings` GROUP BY category ORDER BY minAmountCat ASC LIMIT 1") as SummaryResults;
+    static async listAll(): Promise<MatchRecord[]> {
+            const [results] = await pool.execute("SELECT * FROM `results`") as MatchRecordResults;
 
-        return {
-            ...sum[0],
-            ...latest[0],
-            ...categoryMost[0],
-            ...categoryLeast[0],
-            ...monthMost[0],
-            ...monthLeast[0],
+            results.forEach((expense: any) => {
+                const parsedDate: Date = new Date(expense.month);
+                parsedDate.setDate(parsedDate.getDate() + 1);
+                expense.month = parsedDate.toISOString().split('T')[0];
+            });
+
+            return results.map(obj => new MatchRecord(obj));
         };
-    };
-
-    static async getYearSummary(year: string | undefined): Promise<Summary> {
-        const [sum] = await pool.execute("SELECT SUM(cost) AS sum FROM `spendings` WHERE YEAR(month) = :year", {
-            year: year,
-        }) as SummaryResults;
-        const [latest] = await pool.execute("SELECT name as latest FROM `spendings` WHERE YEAR(month) = :year ORDER BY month DESC LIMIT 1", {
-            year: year,
-        }) as SummaryResults;
-        const [monthMost] = await pool.execute("SELECT MONTHNAME(MAX(month)) AS monthMost, SUM(cost) AS maxAmountMonth FROM `spendings` WHERE YEAR(month) = :year GROUP BY YEAR(month), MONTH(month) ORDER BY maxAmountMonth DESC LIMIT 1", {
-            year: year,
-        }) as SummaryResults;
-        const [monthLeast] = await pool.execute("SELECT MONTHNAME(MAX(month)) AS monthLeast, SUM(cost) AS minAmountMonth FROM `spendings` WHERE YEAR(month) = :year GROUP BY YEAR(month), MONTH(month) ORDER BY minAmountMonth ASC LIMIT 1", {
-            year: year,
-        }) as SummaryResults;
-        const [categoryMost] = await pool.execute("SELECT category AS categoryMost, SUM(cost) AS maxAmountCat FROM `spendings` WHERE YEAR(month) = :year GROUP BY category ORDER BY maxAmountCat DESC LIMIT 1", {
-            year: year,
-        }) as SummaryResults;
-        const [categoryLeast] = await pool.execute("SELECT category AS categoryLeast, SUM(cost) AS minAmountCat FROM `spendings` WHERE YEAR(month) = :year GROUP BY category ORDER BY minAmountCat ASC LIMIT 1", {
-            year: year,
-        }) as SummaryResults;
-
-        return {
-            ...sum[0],
-            ...latest[0],
-            ...categoryMost[0],
-            ...categoryLeast[0],
-            ...monthMost[0],
-            ...monthLeast[0],
-        };
-    };
-
-    static async getMonthSummary(year: string | undefined, month: string | undefined): Promise<SummaryMonth> {
-        const monthNumber = format(new Date(`${month} 1, ${year}`), 'M');
-
-        const [sum] = await pool.execute(
-            "SELECT SUM(cost) AS sum FROM `spendings` WHERE YEAR(month) = :year AND MONTH(month) = :month",
-            { year: year, month: monthNumber }
-        ) as SummaryResultsMonth;
-
-        const [latest] = await pool.execute(
-            "SELECT name AS latest, cost FROM `spendings` WHERE YEAR(month) = :year AND MONTH(month) = :month ORDER BY cost DESC LIMIT 1",
-            { year: year, month: monthNumber }
-        ) as SummaryResultsMonth;
-
-        const [categoryMost] = await pool.execute(
-            "SELECT category AS categoryMost, SUM(cost) AS maxAmountCat FROM `spendings` WHERE YEAR(month) = :year AND MONTH(month) = :month GROUP BY category ORDER BY maxAmountCat DESC LIMIT 1",
-            { year: year, month: monthNumber }
-        ) as SummaryResultsMonth;
-
-        const [categoryLeast] = await pool.execute(
-            "SELECT category AS categoryLeast, SUM(cost) AS minAmountCat FROM `spendings` WHERE YEAR(month) = :year AND MONTH(month) = :month GROUP BY category ORDER BY minAmountCat ASC LIMIT 1",
-            { year: year, month: monthNumber }
-        ) as SummaryResultsMonth;
-
-        return {
-            ...sum[0],
-            ...latest[0],
-            ...categoryMost[0],
-            ...categoryLeast[0],
-        };
-    };
-
-
-static async listAll(): Promise<MatchRecord[]> {
-        const [results] = await pool.execute("SELECT * FROM `spendings`") as ExpensesRecordResults;
-
-        results.forEach((expense: any) => {
-            const parsedDate: Date = new Date(expense.month);
-            parsedDate.setDate(parsedDate.getDate() + 1);
-            expense.month = parsedDate.toISOString().split('T')[0];
-        });
-
-        return results.map(obj => new MatchRecord(obj));
-    };
 
     static async getOne(id: string): Promise<MatchRecord> | null {
-        const [results] = await pool.execute("SELECT * FROM `spendings` WHERE `id` = :id", {
+        const [results] = await pool.execute("SELECT * FROM `results` WHERE `id` = :id", {
             id,
-        }) as ExpensesRecordResults;
+        }) as MatchRecordResults;
 
         results.forEach((expense: any) => {
             const parsedDate: Date = new Date(expense.month);
@@ -153,29 +56,19 @@ static async listAll(): Promise<MatchRecord[]> {
             this.id = uuid()
         }
 
-        await pool.execute("INSERT INTO `spendings` VALUES(:id, :category, :name, :cost, :month, :notes)", {
+        await pool.execute("INSERT INTO `results` VALUES(:id, :player1, :player2, :gameTime, :numberOfMoves, :winner, :date)", {
             id: this.id,
-            category: this.category,
-            name: this.name,
-            cost: this.cost,
-            month: this.month,
-            notes: this.notes,
+            player1: this.player1,
+            player2: this.player2,
+            gameTime: this.gameTime,
+            numberOfMoves: this.numberOfMoves,
+            winner: this.winner,
+            date: this.date,
         });
     };
 
-    async updateRecord(body: ExpenseEntity): Promise<void> {
-        await pool.execute("UPDATE `spendings` SET `category` = :category, `name` = :name, `cost` = :cost, `month` = :month, `notes` = :notes WHERE `id` = :id", {
-            id: this.id,
-            category: body.category,
-            name: body.name,
-            cost: body.cost,
-            month: body.month,
-            notes: body.notes,
-        });
-    }
-
     async delete(): Promise<void> {
-        await pool.execute("DELETE FROM `spendings` WHERE `id` = :id", {
+        await pool.execute("DELETE FROM `results` WHERE `id` = :id", {
             id: this.id
         });
     };
